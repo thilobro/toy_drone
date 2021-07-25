150dfc0de0f1376b521001bd2cd5b545d99b863b
thilo <thilo@kiteswarms.com>
2021-07-25 17:56:17 +0200

    add first tests for kalman filter

--- test/extended_kalman_filter_test.py
+++ test/extended_kalman_filter_test.py
@@ -1,5 +1,37 @@
 from toy_drone.extended_kalman_filter import ExtendedKalmanFilter
+import casadi as ca
 
 
 def test_constructor():
-    pass
+    test_kalman_filter = ExtendedKalmanFilter(ca.vertcat(0), ca.vertcat(0),
+                                              ca.vertcat(1), ca.vertcat(1),
+                                              lambda x, u: ca.vertcat(x + u),
+                                              lambda x, u: ca.vertcat(x),
+                                              lambda x, u: ca.vertcat(1),
+                                              lambda x, u: ca.vertcat(1), 1)
+    assert 0 == test_kalman_filter.get_estimate()
+
+
+def test_input_measurement():
+    test_kalman_filter = ExtendedKalmanFilter(ca.vertcat(0), ca.vertcat(0),
+                                              ca.vertcat(1), ca.vertcat(1),
+                                              lambda x, u: ca.vertcat(x + u),
+                                              lambda x, u: ca.vertcat(x),
+                                              lambda x, u: ca.vertcat(1),
+                                              lambda x, u: ca.vertcat(1), 1)
+    test_kalman_filter.input_measurement(0)
+    assert 0 == test_kalman_filter.get_estimate()
+    test_kalman_filter.input_measurement(1)
+    assert 0 < test_kalman_filter.get_estimate()
+
+
+def test_input_controls():
+    test_kalman_filter = ExtendedKalmanFilter(ca.vertcat(0), ca.vertcat(0),
+                                              ca.vertcat(1), ca.vertcat(1),
+                                              lambda x, u: ca.vertcat(x + u),
+                                              lambda x, u: ca.vertcat(x),
+                                              lambda x, u: ca.vertcat(1),
+                                              lambda x, u: ca.vertcat(1), 1)
+    test_kalman_filter.input_controls(0)
+    test_kalman_filter.input_measurement(0)
+    assert 0 == test_kalman_filter.get_estimate()
