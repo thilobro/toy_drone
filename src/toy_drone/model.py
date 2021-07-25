7c454e29912fa67b1bf851cd6ffc61e3ae45db1f
thilo <thilo@kiteswarms.com>
2021-07-25 21:52:47 +0200

    add docstrings for model and nmpc

--- src/toy_drone/model.py
+++ src/toy_drone/model.py
@@ -6,41 +6,50 @@ from toy_drone.utils import rk4
 
 
 class ToyDroneModel():
-    # TODO: docstrings
+    """
+    ToyDroneModel Model of a 2d drone with two propellers. States are position, velocity,
+    orientation and angular velocity. Controls are two forces of the props.
+    """
 
     def __init__(self, parameters):
+        """
+        __init__ Constructor
+
+        Args:
+            parameters: dictionary of model paramters
+        """
         self._state_dim = 6
         self._controls_dim = 2
         self._parameters = parameters
         self._state = np.zeros(6)
         self._controls = np.zeros(2)
         self._build_model_functions()
-        # state consists of 2 positions, 2 velocities, 1 angle, one angular velocity
-        # controls consists of 2 forces
 
     def _build_model_functions(self):
+        """
+        _build_model_functions Build the casadi functions for ODE, sensor model and jacobians.
+        """
+
         state = ca.SX.sym("state", self._state_dim)
         controls = ca.SX.sym("controls", self._controls_dim)
-        # TODO: how to manage disturbance states?
-        # disturbancce_state = ca.SX.sym("disturbance_state", 9)
 
         self._ode = self._build_ode(state, controls)
-        self._disturbed_ode = self._build_disturbed_ode(state, controls)
         self._sensor_model = self._build_sensor_model(state, controls)
         self._state_jacobian = self._build_state_jacobian(state, controls)
         self._control_jacobian = self._build_control_jacobian(state, controls)
         self._sensor_jacobian = self._build_sensor_jacobian(state, controls)
 
-    def _build_disturbed_ode(self, state, controls):
-        # TODO: build ode with disturbance states, acceleration and torque disturbance
-        disturbances = ca.SX.sym("disturbances", 3)
-        disturbed_state = ca.vertcat(state, disturbances)
-        disturbed_ode = ca.Function("disturbed_ode", [disturbed_state, controls],
-                                    [ca.vertcat(self._ode(disturbed_state[:-3],
-                                                          controls), 0, 0, 0)])
-        return disturbed_ode
-
     def _build_ode(self, state, controls):
+        """
+        _build_ode Build the ode of the model in the form of xdot = f(x, u) as a casadi function.
+
+        Args:
+            state: symbolic state
+            controls: symbolic controls
+
+        Returns:
+            casadi ode function
+        """
         velocity = state[2:4]
         angle = state[4]
         angular_velocity = state[5]
@@ -58,33 +67,89 @@ class ToyDroneModel():
         return ca.Function("ode", [state, controls], [dstate])
 
     def _build_sensor_model(self, state, controls):
-        # GPS + gyro + accelerometer
+        """
+        _build_sensor_model Build the sensor model as a casadi function. All sensors (GPS, gyro,
+        accelerometer) are in one sensor equation of the form y = h(x, u).
+
+        Args:
+            state: symbolic state
+            controls: symbolic controls
+
+        Returns:
+            casadi sensor model function
+        """
         angle = state[4]
         acceleration = 1.0/self._parameters["mass"] * ((controls[0] + controls[1])
                                                        * ca.vertcat(ca.sin(angle), -ca.cos(angle))
                                                        + ca.vertcat(0, self._parameters["gravity"]))
-        sensor_values = ca.vertcat(state[:2], state[5], acceleration)
+        gps = state[:2]
+        gyro = state[5]
+        sensor_values = ca.vertcat(gps, gyro, acceleration)
         return ca.Function("sensor_model", [state, controls], [sensor_values])
 
     def _build_state_jacobian(self, state, controls):
+        """
+        _build_state_jacobian Build casadi function of the state jacobian df/dx(x, u) at state x
+        and control u.
+
+        Args:
+            state: symbolic state
+            controls: symbolic controls
+
+        Returns:
+            casadi state jacobian function
+        """
         return ca.Function("state_jacobian", [state, controls],
                            [ca.jacobian(self._ode(state, controls), state)])
 
     def _build_control_jacobian(self, state, controls):
+        """
+        _build_control_jacobian Build casadi function of the control jacobian df/du(x, u) at state x
+        and contol u.
+
+        Args:
+            state: symbolic state
+            controls: symbolic controls
+
+        Returns:
+            casadi control jacobian function
+        """
         return ca.Function("control_jacobian", [state, controls],
                            [ca.jacobian(self._ode(state, controls), controls)])
 
     def _build_sensor_jacobian(self, state, controls):
+        """
+        _build_sensor_jacobian Build casadi function of the sensor jacobian dh/dx(x, u) at state x
+        and control u.
+
+        Args:
+            state: symbolic state
+            controls: symbolic controls
+
+        Returns:
+            casadi sensor jacobian function
+        """
         return ca.Function("sensor_jacobian", [state, controls],
                            [ca.jacobian(self._sensor_model(state, controls), state)])
 
-    def get_sensor_model(self):
-        return self._sensor_model
-
     def make_step(self, controls, dt):
+        """
+        make_step Integrate the model forward for time step dt with rk4. Clip inputs to simulate
+        physical constraints of the model.
+
+        Args:
+            controls: control inputs (zero-order-hold)
+            dt: integration time step
+
+        Returns:
+            new state
+        """
         self._controls = np.clip(controls, 0, self._parameters["max_force_input"])
         self._state = rk4(self._ode, self._state, self._controls, dt).full().squeeze()
         return self._state
+
+    def get_sensor_model(self):
+        return self._sensor_model
 
     def get_ode(self):
         return self._ode
--- src/toy_drone/nmpc.py
+++ src/toy_drone/nmpc.py
@@ -4,9 +4,27 @@ from toy_drone.utils import rk4
 
 
 class Nmpc():
-    # TODO: docstrings
+    """
+    Nmpc Nonlinear Model Predictive Controller with quadratic running and terminal cost.
+    Additional quadratic cost on changes in the controls to penalize spikes.
+    Ode discretized with rk4 and multiple shooting constraints.
+    """
+
     def __init__(self, model, state_running_cost, state_terminal_cost,
                  control_running_cost, dcontrol_running_cost, control_bounds, dt, N):
+        """
+        __init__ Constructor
+
+        Args:
+            model: model for the system that nmpc is controlling
+            state_running_cost: tuning matrix for quadratic state cost
+            state_terminal_cost: tuning matrix for quadratic terminal state cost
+            control_running_cost: tuning matrix for quadratic control cost
+            dcontrol_running_cost: tuning matrix for quadratic cost on control changes
+            control_bounds: upper and lower bounds on the controls
+            dt: discretization time step
+            N: nmpc horizon length
+        """
         self._ode = model.get_ode()
         self._state_dim = model.get_state_dim()
         self._controls_dim = model.get_controls_dim()
@@ -22,23 +40,58 @@ class Nmpc():
                                    * self._horizon + self._state_dim)
 
     def _model_step(self, state, controls):
+        """
+        _model_step Integrate the model forward.
+
+        Args:
+            state: symbolic state
+            controls: symbolic controls
+        """
         return rk4(self._ode, state, controls, self._dt)
 
     @staticmethod
     def _quadratic_norm(vector, matrix):
+        """
+        _quadratic_norm Compute the quadratic norm x.T Q x for a vector x and a matrix Q.
+        """
         return ca.mtimes(ca.mtimes(vector.T, matrix), vector)
 
     def _running_cost(self, state, controls, dcontrols, reference):
+        """
+        _running_cost Compute running cost for a single time step.
+
+        Args:
+            state: symbolic state at the time step
+            controls: symbolic controls at the time step
+            dcontrols: symbolic control difference at the time step
+            reference: symbolic referenc at the time step
+
+        Returns:
+            running cost term for single time step
+        """
         cost = self._quadratic_norm(state - reference, self._Q)\
             + self._quadratic_norm(controls, self._R) + self._quadratic_norm(dcontrols, self._dR)
         return cost
 
     def _terminal_cost(self, state, reference):
+        """
+        _terminal_cost Compute terminal cost.
+
+        Args:
+            state: symbolic state at last time step
+            reference: symbolic reference at last time step
+
+        Returns:
+            Terminal cost term
+        """
         cost = self._quadratic_norm(state - reference, self._Q_term)
 
         return cost
 
     def _build_ocp(self):
+        """
+        _build_ocp Build up OCP out of casadi expressions and generate numerical solver.
+        """
         ocp_variables = []
         ocp_variables += [ca.SX.sym('V0', self._state_dim + self._controls_dim)]
         params = []
@@ -84,6 +137,16 @@ class Nmpc():
         self._solver = ca.nlpsol('solver', 'ipopt', nlp, solver_options)
 
     def _solve_ocp(self, state, reference):
+        """
+        _solve_ocp Solve the OCP for a set of parameters.
+
+        Args:
+            state: current state that is used as initial constraint for the OCP
+            reference: reference of length of the horizon that should be tracked
+
+        Returns:
+            Optimal trajectory in state and controls
+        """
         lbx = [-ca.inf] * self._state_dim + [self._control_bounds[0]] * self._controls_dim
         lbx = lbx * (self._horizon) + [-ca.inf] * self._state_dim
         ubx = [ca.inf] * self._state_dim + [self._control_bounds[1]] * self._controls_dim
@@ -93,12 +156,34 @@ class Nmpc():
         return solution['x']
 
     def _extract_initial_guess(self, optimal_trajectory):
+        """
+        _extract_initial_guess Extract the initial guess for the next iteration by shifting the
+        optimal trajectory to the left by one time step and duplicating the last time step.
+        Since we don't have controls for the last time step, we fill the up with zeros.
+
+        Args:
+            optimal_trajectory: Optimal state and controls trajectory
+
+        Returns:
+            Initial guess for the next iteration
+        """
         initial_guess = optimal_trajectory[self._state_dim + self._controls_dim:]
         initial_guess = ca.vertcat(initial_guess, [0] * self._controls_dim)
         initial_guess = ca.vertcat(initial_guess, optimal_trajectory[-(self._state_dim):])
         return initial_guess
 
     def compute_control(self, state, reference):
+        """
+        compute_control Compute an nmpc control output. Solve the OCP and returns the first control
+        of the solution. The rest is used to compute the new initial guess for the next iteration.
+
+        Args:
+            state: state at which the control should be computed
+            reference: reference for the horizon
+
+        Returns:
+            Optimal nmpc control
+        """
         # solve OCP
         optimal_trajectory = self._solve_ocp(state, reference)
         # extract V_initial for next iteration
